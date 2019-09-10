63e09d4944531e86f7b3d5a0d20127427c2d395a
Vinicius Cardoso <vinny.cardoso96@gmail.com>
2019-09-10 19:38:52 -0300

    Updates

---
+++ precache-manifest.d055b1481d13e08ba7667d7ab482266a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067b16be78bae4490d9861aac7c4546f",
+    "url": "/responsiveportfolio/index.html"
+  },
+  {
+    "revision": "4a4b181160e0cb63cc4a",
+    "url": "/responsiveportfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7a77354fb01d675bd629",
+    "url": "/responsiveportfolio/static/js/2.387e0d42.chunk.js"
+  },
+  {
+    "revision": "4a4b181160e0cb63cc4a",
+    "url": "/responsiveportfolio/static/js/main.46702744.chunk.js"
+  },
+  {
+    "revision": "780086cc110e737427cd",
+    "url": "/responsiveportfolio/static/js/runtime~main.472bc173.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/responsiveportfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
