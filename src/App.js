37d5726ddcf2602b6f39339423c10676a104935a
Vinicius Cardoso <vinny.cardoso96@gmail.com>
2019-09-10 21:31:18 -0300

    update in main menu

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
   return (
     <div className="container-fluid">
      <nav className="navbar navbar-expand-md navbar-dark bg-light">
-       
-       <a href="#">Home</a>
-       <a href="#">Channel</a>
-       <a href="#">Login</a>
+      <a className="navbar-brand" href="#"><i class="fab fa-react"></i>&nbsp;Full-Stack Developer Vinicius C.</a>
+       <div className="navbar-nav">
+       <a className="nav-item nav-link" href="#">Home</a>
+       <a className="nav-item nav-link" href="#">Channel</a>
+       <a className="nav-item nav-link" href="#">Login</a>
+       </div>
      </nav>
     </div>
   );
