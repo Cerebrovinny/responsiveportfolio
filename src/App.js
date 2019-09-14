59c32da3561972328b88c1f7df9495822358b6c5
Vinicius Cardoso <vinny.cardoso96@gmail.com>
2019-09-14 17:56:11 -0300

    fix for menu responsive

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="container-fluid p-0">
-      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+class App extends Component {
+  return() {
+    return (
 
-        <a className="navbar-brand" href="#"><i className="fab fa-react"></i>&nbsp;Full-Stack Developer Vinicius C.</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMainToggler"
-          aria-controls="navbarMainToggler" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span> // botao responsivo
-        </button>
+      <div className="container-fluid p-0">
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
 
-        <section className="collapse" id="navbarMainToggler">
-          <div className="navbar-nav ml-auto">
-            <a className="nav-item nav-link" href="#">Home</a>
-            <a className="nav-item nav-link" href="#">Channel</a>
-            <a className="nav-item nav-link" href="#">Login</a>
-          </div>
-        </section>
+          <a className="navbar-brand" href="#"><i className="fab fa-react"></i>&nbsp;Full-Stack Developer Vinicius C.</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMainToggler"
+            aria-controls="navbarMainToggler" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <section className="collapse" id="navbarMainToggler">
+            <div className="navbar-nav ml-auto">
+              <a className="nav-item nav-link" href="#">Home</a>
+              <a className="nav-item nav-link" href="#">Channel</a>
+              <a className="nav-item nav-link" href="#">Login</a>
+            </div>
+          </section>
 
 
 
-      </nav>
-    </div>
-  );
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default App;
