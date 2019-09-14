1384a15e2c9ae48330893e91df9ab143bb1b7067
Vinicius Cardoso <vinny.cardoso96@gmail.com>
2019-09-14 19:09:04 -0300

    some changes for clean code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 class App extends Component {
-  return() {
+  render() {
     return (
-
-      <div className="container-fluid p-0">
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-
-          <a className="navbar-brand" href="#"><i className="fab fa-react"></i>&nbsp;Full-Stack Developer Vinicius C.</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMainToggler"
-            aria-controls="navbarMainToggler" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <section className="collapse" id="navbarMainToggler">
-            <div className="navbar-nav ml-auto">
-              <a className="nav-item nav-link" href="#">Home</a>
-              <a className="nav-item nav-link" href="#">Channel</a>
-              <a className="nav-item nav-link" href="#">Login</a>
-            </div>
-          </section>
-
-
-
-        </nav>
-      </div>
+      <Navbar>
+      </Navbar>
     );
   }
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+
+class Navbar extends Component {
+    render() {
+        return (
+
+            <div className="container-fluid p-0">
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+
+                    <a className="navbar-brand" href="#"><i className="fab fa-react"></i>&nbsp;Full-Stack Developer Vinicius C.</a>
+
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMainToggler"
+                        aria-controls="navbarMainToggler" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <section className="collapse navbar-collapse" id="navbarMainToggler">
+                        <div className="navbar-nav ml-auto">
+                            <a className="nav-item nav-link" href="#">Home</a>
+                            <a className="nav-item nav-link" href="#">Channel</a>
+                            <a className="nav-item nav-link" href="#">Login</a>
+                        </div>
+                    </section>
+
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
