58f355b0385218d61c37751a86aff9dffac1c27a
Vinicius Cardoso <vinny.cardoso96@gmail.com>
2019-09-14 17:35:53 -0300

    Fix for menu

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="container-fluid">
-     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-      <a className="navbar-brand" href="#"><i class="fab fa-react"></i>&nbsp;Full-Stack Developer Vinicius C.</a>
-       <div className="navbar-nav">
-       <a className="nav-item nav-link" href="#">Home</a>
-       <a className="nav-item nav-link" href="#">Channel</a>
-       <a className="nav-item nav-link" href="#">Login</a>
-       </div> 
-       <button className="navbar-toggler" type="button" data-togler="collapse" data-target="#navbarMainToggler"
-      aria-controls="navbarMainToggler" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span> // botao responsivo
-      </button>
-     </nav>
+    <div className="container-fluid p-0">
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+
+        <a className="navbar-brand" href="#"><i className="fab fa-react"></i>&nbsp;Full-Stack Developer Vinicius C.</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMainToggler"
+          aria-controls="navbarMainToggler" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span> // botao responsivo
+        </button>
+
+        <section className="collapse" id="navbarMainToggler">
+          <div className="navbar-nav ml-auto">
+            <a className="nav-item nav-link" href="#">Home</a>
+            <a className="nav-item nav-link" href="#">Channel</a>
+            <a className="nav-item nav-link" href="#">Login</a>
+          </div>
+        </section>
+
+
+
+      </nav>
     </div>
   );
 }
